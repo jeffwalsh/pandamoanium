9412eae826b18c589adf2c43fa314802c4b45c1c
Jeffery Walsh <rlxrlps@gmail.com>
2022-04-05 21:13:17 -0700

    .

--- src/client/src/components/Nav.svelte
+++ src/client/src/components/Nav.svelte
@@ -1,11 +1,14 @@
 <script>
-const { default: Logo }=require("./Logo.svelte");
+  import { push } from "svelte-spa-router";
 
+  const { default: Logo } = require("./Logo.svelte");
 </script>
 
 <div class="container">
-    <div class="nav flex justify-between content-center">
-      <div class="logo" on:click={async () => await push("/")}> <Logo/>  </div>
-      <div class="leaderboard" on:click={async () => await leaderboard()}> Leaderboard </div>
+  <div class="nav flex justify-between content-center">
+    <div class="logo" on:click={async () => await push("/")}><Logo /></div>
+    <div class="leaderboard" on:click={async () => await push("/leaderboard")}>
+      Leaderboard
     </div>
   </div>
+</div>
